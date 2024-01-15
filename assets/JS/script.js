5506d31ffbf806a3b0973b659c9dd5d46ffc6e37
rodrigoc-santos <rodrigocoelho.20rsc@gmail.com>
2024-01-15 14:56:23 -0300

    Adicionando recursividade e adaptando a função para alterar o nome do botão

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,4 +1,11 @@
 function toogleMode() {
     const html = document.documentElement
     html.classList.toggle('clicked')
+
+    const button = document.getElementById('botao')
+    if(html.classList.contains('clicked')) {
+        button.textContent = 'Close'
+    } else {
+        button.textContent = 'Open'
+    }
 }
