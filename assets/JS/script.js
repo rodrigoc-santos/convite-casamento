9330e477f3aec4b53e50515fd72b1e0c265ebcad
rodrigoc-santos <rodrigocoelho.20rsc@gmail.com>
2024-01-15 17:14:08 -0300

    Adição da contagem regressiva

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,5 +1,35 @@
+const daysEL = document.getElementById('days')
+const hoursEL = document.getElementById('hours')
+const minEL = document.getElementById('min')
+const secsEL = document.getElementById('secs')
+const html = document.documentElement
+const wedding = '2024-09-13'
+
+function countdow () {
+    const weddingDate = new Date(wedding)
+    const currentDate = new Date()
+    const totalSeconds = (weddingDate - currentDate) / 1000
+
+    const days = Math.floor(totalSeconds / 3600 / 24)
+    const hours = Math.floor(totalSeconds / 3600) % 24
+    const mins = Math.floor(totalSeconds / 60) % 60
+    const seconds = Math.floor(totalSeconds) % 60
+
+    daysEL.innerHTML = days
+    hoursEL.innerHTML = formatTime(hours)
+    minEL.innerHTML = formatTime(mins)
+    secsEL.innerHTML = formatTime(seconds)
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time
+}
+
+countdow()
+setInterval(countdow, 1000)
+
+
 function toogleMode() {
-    const html = document.documentElement
     html.classList.toggle('clicked')
 
     const button = document.getElementById('botao')
